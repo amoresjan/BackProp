28851bda7f55f76d77112be7d07987fb3c0009d7
Jan Dale Amores <amoresjan14@gmail.com>
2021-02-21 11:16:50 +0800

    Add project files.

---
+++ Gutomlearning/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Gutomlearning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 273);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 47);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Identify";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 38);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Save File";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 92);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 47);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Load Neurons";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog1";
+            this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(11, 68);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(67, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.Visible = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 11);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Learn";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 24);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Result: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(371, 331);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Face Recognition";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Gutomlearning/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+namespace Gutomlearning
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet neuralNetwork = new NeuralNet(40000, 100, 1);
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            neuralNetwork.loadWeights("C:\\Users\\amore\\Desktop\\3rd");
+        }
+
+        //for training
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int epoch = 0; epoch < Convert.ToInt32(textBox4.Text); epoch++)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                for (int i = 0; i < 20; i++)
+                {
+                    int count = 0;
+                    Bitmap image = new Bitmap("C:\\Users\\amore\\Documents\\BackPropagation\\BackPropagation\\Images\\old\\" + i.ToString() + ".png");
+                    
+                    for (int width = 0; width < image.Width; width++)
+                    {
+                        for (int height = 0; height < image.Height; height++)
+                        {
+                            Color pixel = image.GetPixel(wid    th, height);
+                            neuralNetwork.setInputs(count, ((pixel.R + pixel.G + pixel.B) / 3));
+                            count++;
+                        }
+                    }
+                    neuralNetwork.setDesiredOutput(0, 1.0);
+                    neuralNetwork.learn();
+                }
+                for (int i = 0; i < 20; i++)
+                {
+                    int count = 0;
+                    Bitmap image = new Bitmap("C:\\Users\\amore\\Documents\\BackPropagation\\BackPropagation\\Images\\young\\" + i.ToString() + ".png");
+
+                    for (int width = 0; width < image.Width; width++)
+                    {
+                        for (int height = 0; height < image.Height; height++)
+                        {
+                            Color pixel = image.GetPixel(width, height);
+                            neuralNetwork.setInputs(count, ((pixel.R + pixel.G + pixel.B) / 3));
+                            count++;
+                        }
+                    }
+                    neuralNetwork.setDesiredOutput(0, 0.0);
+                    neuralNetwork.learn();
+                }
+                stopwatch.Stop();
+                Console.WriteLine("Epoch " + epoch.ToString() + " time elapsed: " + stopwatch.Elapsed);
+
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            
+        }
+
+        //for identifying image
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Bitmap image = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = image;
+            int count = 0;
+            for (int w = 0; w < image.Width; w++)
+            {
+                for (int h = 0; h < image.Height; h++)
+                {
+                    Color pixel = image.GetPixel(w, h);
+                    double grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    neuralNetwork.setInputs(count, grey);
+                    count++;
+                }
+            }
+            neuralNetwork.run();
+           // textBox3.Text = "" + dukasico.getOuputData(0);
+            double f = neuralNetwork.getOuputData(0);
+            if (f < 0.5)
+                label1.Text = "Result: Not a Senior Citizen";
+            else
+                label1.Text = "Result: Senior Citizen";
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            neuralNetwork.saveWeights(saveFileDialog1.FileName);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.ShowDialog();
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+            neuralNetwork.loadWeights(openFileDialog2.FileName);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
